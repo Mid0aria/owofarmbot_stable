beba4766c6ff29e38b0862e2def008327b98bb89
Random-629671 <117952564+Random-629671@users.noreply.github.com>
2024-11-19 13:57:42 +0700

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,77 +1,66 @@
 let reallog = [];
 let client, extrac;
 module.exports = (uwu) => { //plz change it when upload, u will remember it, right?
-    const chalk = require("chalk");
     let logger = [];
-    let length = uwu.config.settings.loglength;
-    if (uwu.basic.enable) extrac = uwu;
-    else client = uwu;
-
+    let length = uwu ? uwu.config.settings.loglength : 16;
+    if (uwu.global.type == "Main") client = uwu;
+    else extrac = uwu;
+    
     function info(type, module, result = "") {
-        reallog.push(
-            chalk.white(
-                `[${new Date().toLocaleTimeString()}]`
-            ) + " " +
-            chalk.blue(chalk.bold(type)) + chalk.white(` >> `) +
-            chalk.cyan(chalk.bold(uwu.global.type)) +
-            chalk.white(` > `) +
-            chalk.magenta(module) +
-            chalk.white(` > `) +
-            chalk.green(result)
-        );
-        if (reallog.length >= length) reallog.shift();
-        showlog(reallog);
-    };
+        logging(type, module, result, client.chalk.green);
+    }
 
     function warn(type, module, result = "") {
-        reallog.push(
-            chalk.white(
-                `[${new Date().toLocaleTimeString()}]`
-            ) + " " +
-            chalk.blue(chalk.bold(type)) + chalk.white(` >> `) +
-            chalk.cyan(chalk.bold(uwu.global.type)) +
-            chalk.white(` > `) +
-            chalk.magenta(module) +
-            chalk.white(` > `) +
-            chalk.yellow(result)
-        );
-        if (reallog.length >= length) reallog.shift();
-        showlog(reallog);
-    };
+        logging(type, module, result, client.chalk.yellow);
+    }
 
     function alert(type, module, result = "") {
-        reallog.push(
-            chalk.white(
-                `[${new Date().toLocaleTimeString()}]`
-            ) + " " +
-            chalk.blue(chalk.bold(type)) + chalk.white(` >> `) +
-            chalk.cyan(chalk.bold(uwu.global.type)) +
-            chalk.white(` > `) +
-            chalk.magenta(module) +
-            chalk.white(` > `) +
-            chalk.red(result)
-        );
+        logging(type, module, result, client.chalk.red);
+    }
+
+    function logging(type, module, result, color) {
+        const logMessage = `${client.chalk.white(`[${new Date().toLocaleTimeString()}]`)} ` +
+                           `${client.chalk.blue(client.chalk.bold(type))}` +
+                           `${(type == "Bot" || type == "Updater") && module != "Startup" ? "" : //idk why i put this on
+                           `${client.chalk.white(" >> ")}${client.chalk.cyan(client.chalk.bold(uwu.global.type))}`} > ` +
+                           `${client.chalk.magenta(module)} > ` +
+                           `${color(result)}`;
+
+        reallog.push(logMessage);
         if (reallog.length >= length) reallog.shift();
         showlog(reallog);
-    };
+    }
 
     function showlog(reallog) {
+        //no client
+        if (!client.global.temp.isready || !extrac.global.temp.isready) {
+            console.log(reallog[reallog.length - 1]);
+            return;
+        }
+        
+        //leave the var here if future need
         var mainHunt = client.global.total.hunt;
         var mainBattle = client.global.total.battle;
-        var mainEvent = client.global.gems.isevent;
+        var mainEvent = client.global.gems.isevent ? "Yes" : "No";
         var mainCF = client.global.gamble.coinflip;
         var mainSlot = client.global.gamble.slot;
         var mainCow = client.global.gamble.cowoncywon;
+        var mainCaptcha = client.global.captchadetected ? client.chalk.red("Danger  ") : client.chalk.green("Safe    ");
+        var mainPause = client.global.paused ? client.chalk.yellow("Paused  ") : client.chalk.cyan("Running ");
         
-        var extraHunt = extrac.global.total.hunt;
-        var extraBattle = extrac.global.total.battle;
-        var extraEvent = extrac.global.gems.isevent;
-        var extraCF = extrac.global.gamble.coinflip;
-        var extraSlot = extrac.global.gamble.slot;
-        var extraCow = extrac.global.gamble.cowoncywon;
+        if (extrac) {
+            var extraHunt = extrac.global.total.hunt;
+            var extraBattle = extrac.global.total.battle;
+            var extraEvent = extrac.global.gems.isevent ? "Yes" : "No";
+            var extraCF = extrac.global.gamble.coinflip;
+            var extraSlot = extrac.global.gamble.slot;
+            var extraCow = extrac.global.gamble.cowoncywon;
+            var extraCaptcha = extrac.global.captchadetected ? client.chalk.red("Danger  ") : client.chalk.green("Safe    ");
+            var extraPause = extrac.global.paused ? client.chalk.yellow("Paused  ") : client.chalk.cyan("Running ");
+        }
 
         
-        function padder(value, start) {
+        function padder(value) {
             let paddedText = "";
             let temp;
             if (value != null) temp = value.toString();
@@ -79,56 +68,44 @@ module.exports = (uwu) => { //plz change it when upload, u will remember it, rig
             temp = temp.trim();
             
             let length = temp.length;
-            
-            if (start == true) {
-                if (temp.length < 8) {
-                    paddedText = temp.padStart(7, ' ');
-                    return paddedText;
-                } else {
-                    paddedText = temp.slice(0, 5).padStart(6, ' ') + "+";
-                    return paddedText;
-                }
+            if (temp.length < 9) {
+                paddedText = temp.padEnd(8, ' ');
+                return paddedText;
             } else {
-                if (temp.length < 8) {
-                    paddedText = temp.padEnd(7, ' ');
-                    return paddedText;
-                } else {
-                    paddedText = temp.slice(0, 5).padEnd(6, ' ') + "+";
-                    return paddedText;
-                }
+                paddedText = temp.slice(0, 6).padEnd(7, ' ') + "+";
+                return paddedText;
             }
         }
         
-        if (uwu.config.extra.enable && uwu.config.settings.newlog) {
+        if (uwu.config.extra.enable && extrac && uwu.config.settings.newlog) {
             console.clear();
             console.log(
-`==================================================================
-Status               || Main   ||  Extra
-==================================================================
-Total cowoncy won    || ${padder(mainCow, false)}||${padder(extraCow, true)}
-==================================================================
->>>Questing
-                     || ${client.global.quest.title}
-Main                 || ${client.global.quest.reward}
-                     || ${client.global.quest.progress}
-                     || ${extrac.global.quest.title}
-Extra                || ${extrac.global.quest.reward}
-                     || ${extrac.global.quest.progress}
+`╔══════════╦═══════════════════════╦════════════════════════════════════════════════
+║ Token    ║ Status                ║ Questing
+╠══════════╬═══════════════════════╬════════════════════════════════════════════════
+║ Main     ║ Total hunt: ${padder(mainHunt, false)}  ║ ${client.global.quest.title}
+║ ${mainCaptcha} ║ Total battle: ${padder(mainBattle, false)}║ ${client.global.quest.reward}
+║ ${mainPause} ║ Cowoncy won: ${padder(mainCow, false)} ║ ${client.global.quest.progress}
+╠══════════╬═══════════════════════╬════════════════════════════════════════════════
+║ Extra    ║ Total hunt: ${padder(extraHunt, false)}  ║ ${extrac.global.quest.title}
+║ ${extraCaptcha} ║ Total battle: ${padder(extraBattle, false)}║ ${extrac.global.quest.reward}
+║ ${extraPause} ║ Cowoncy won: ${padder(extraCow, false)} ║ ${extrac.global.quest.progress}
+╚══════════╩═══════════════════════╩════════════════════════════════════════════════
 >>> Log`);
             for (const logs of reallog) console.log(logs);
         } else if (uwu.config.settings.newlog) {
             console.clear();
             console.log(
-`==================================================================
-Status               || Value
-==================================================================
-Total hunt           || ${client.global.total.hunt}
-Total battle         || ${client.global.total.battle}
-Having event         || ${client.global.gems.isevent}
-Total coinflip       || ${client.global.gamble.coinflip}
-Total slot           || ${client.global.gamble.slot}
-Total cowoncy won    || ${client.global.gamble.cowoncywon}
-==================================================================
+`╔══════════════════════╦══════════╦═══════════════════════════════════════════════
+║ Name                 ║ Status   ║ Questing
+╠══════════════════════╬══════════╬═══════════════════════════════════════════════
+║ Total hunt           ║ ${padder(mainHunt, false)} ║ > Title
+║ Total battle         ║ ${padder(mainBattle, false)} ║ ${client.global.quest.title} 
+║ Having event         ║ ${padder(mainEvent, false)} ║ > Reward
+║ Total cowoncy won    ║ ${padder(mainCow, false)} ║ ${client.global.quest.reward}
+║ Safety level         ║ ${mainCaptcha} ║ > Progress
+║ Running?             ║ ${mainPause} ║ ${client.global.quest.progress}
+╚══════════════════════╩══════════╩═══════════════════════════════════════════════
 >>> Log`);
             for (const logs of reallog) console.log(logs);
         } else console.log(reallog[reallog.length - 1]);
