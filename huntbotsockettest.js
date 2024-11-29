4ad7bde7e6fad1aeea5b26b3c8f5720ae99c3afb
Mid0aria <infoihsandemir@gmail.com>
2024-11-29 08:38:50 +0300

    isPortInUse

--- huntbotsockettest.js
+++ huntbotsockettest.js
@@ -8,21 +8,50 @@
 
 const io = require("socket.io-client");
 const cp = require("child_process");
+const net = require("net");
 const config = require("./config.json");
-const socket = io(`http://localhost:${config.socket.port}`); // connect to the Python server
 
-cp.spawn("py", ["./utils/huntbot/huntbotcaptcha.py"]); // spawn huntbotcaptcha.py in headless mode
+const port = config.socket.port;
+
+function isPortInUse(port, host = "localhost") {
+    return new Promise((resolve) => {
+        const server = net.createServer();
+
+        server.once("error", (err) => {
+            if (err.code === "EADDRINUSE") {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
+
+        server.once("listening", () => {
+            server.close();
+            resolve(false);
+        });
+
+        server.listen(port, host);
+    });
+}
+
+isPortInUse(port).then((inUse) => {
+    if (inUse) {
+        return 0;
+    } else {
+        cp.spawn("py", ["./utils/huntbot/huntbotcaptcha.py"]);
+    }
+});
+
+const socket = io(`http://localhost:${port}`);
 
 socket.on("connect", () => {
-    console.log("I connected to the Hunt Bot Captcha server");
+    console.log("Hunt Bot Captcha sunucusuna bağlandım.");
 
-    // Send the CAPTCHA URL to the Python server
     const captchaUrl =
-        "https://cdn.discordapp.com/attachments/1267791683865673738/1311700675956179015/captcha.png?ex=6749cff3&is=67487e73&hm=87546f6269e04a7d0078327f86ece562b01e3e98cbfd5d2d4b755059e4b5fcc0&"; // Buraya gerçek CAPTCHA URL'nizi koyun
+        "https://cdn.discordapp.com/attachments/1267791683865673738/1311700675956179015/captcha.png?ex=6749cff3&is=67487e73&hm=87546f6269e04a7d0078327f86ece562b01e3e98cbfd5d2d4b755059e4b5fcc0&";
     socket.emit("captcha", captchaUrl);
 });
 
-// When CAPTCHA solution is received
 socket.on("captcha_solution", (solution) => {
-    console.log("Solved CAPTCHA:", solution);
+    console.log("Çözülen CAPTCHA:", solution);
 });
