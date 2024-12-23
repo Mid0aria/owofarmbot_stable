05f3222a809bc5be2fe14e9f99d91f03ed86af5f
Mid0aria <infoihsandemir@gmail.com>
2024-12-23 15:02:41 +0300

    added uptime infos in stats commands

--- commands/stats.js
+++ commands/stats.js
@@ -12,8 +12,14 @@ module.exports = {
     run: async (client, message, args) => {
         let totals = client.global.total;
 
+        const seconds = Math.floor(process.uptime());
+        const days = Math.floor(seconds / 86400);
+        const hours = Math.floor((seconds % 86400) / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const uptime = `${days}d ${hours}h ${minutes}m ${seconds % 60}s`;
+
         let stats = `
-OwO Farm Bot Stable Statistics:
+OwO Bot Statistics:
 ===================
 - Hunt: ${totals.hunt}
 - Battle: ${totals.battle}
@@ -21,6 +27,7 @@ OwO Farm Bot Stable Statistics:
 - Pray: ${totals.pray}
 - Curse: ${totals.curse}
 ===================
+- Uptime: ${uptime}
         `;
 
         await message.delete();
